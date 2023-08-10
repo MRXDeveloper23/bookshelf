8befa3231d50d7b2d2aaa54dd77d32b5edda1ba2
Khurmatillo <khurmatillomamasoliev@gmail.com>
2023-08-10 19:11:42 +0500

    added md5 hash utility

---
+++ src/utils/md5.ts
@@ -0,0 +1,10 @@
+import md5 from "md5-hash";
+
+export const generateMD5 = (
+  method: string | undefined,
+  url: string | undefined,
+  secret: string | null,
+  body?: string | object
+) => {
+  return md5(`${method?.toUpperCase()}${url}${body}${secret}`);
+};
